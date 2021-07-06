6ecbf6e032a29308b0bbc1ceec175e8e81dd0015
Sota529 <za446131@gmail.com>
2021-07-06 23:39:38 +0900

    🔥 🚑 anyに変更

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@chakra-ui/react";
 import { ReactNode, VFC } from "react";
 
-export const Layout: VFC<{ children: ReactNode }> = (props) => {
+export const Layout: VFC<{ children: any }> = (props) => {
   return (
     <Box mx="50px" mb="100px">
       {props.children}
